02a00b125c9aa911ce98eb934defdcfa2778f9e5
Braulio <braulio@Mac-Pro-de-Braulio.local>
2014-10-06 18:15:06 -0500

    Initial Commit

---
+++ homework-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homework-2
+//
+//  Created by Braulio on 06/10/14.
+//  Copyright (c) 2014 utzmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ homework-2Tests/homework_2Tests.m
@@ -0,0 +1,40 @@
+//
+//  homework_2Tests.m
+//  homework-2Tests
+//
+//  Created by Braulio on 06/10/14.
+//  Copyright (c) 2014 utzmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface homework_2Tests : XCTestCase
+
+@end
+
+@implementation homework_2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
